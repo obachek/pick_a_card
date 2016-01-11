fec4024e4257d6631a7ac45e2d6af71483c60c3f
obachek <obachek@yandex.ru>
2016-01-11 16:45:16 +0300

    start version

---
+++ pick_a_card.rb
@@ -0,0 +1,8 @@
+
+suits = ['chirva', 'buba', 'kresti', 'pika']
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'NS', 'Dama', 'King', 'TUZ']
+
+masti = suits.sample
+carti = values.sample
+
+puts carti + " " + masti
